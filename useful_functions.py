67f604b4921ce8307305e9485d6da45d147d2fcf
gmondaut <giorgio_mondauto@gmail.com>
2020-02-09 21:33:24 +0000

    adding json functions

---
+++ useful_functions.py
@@ -0,0 +1,29 @@
+import json
+import glo
+
+
+# from text to json files
+def process_text_to_json(file_name, topic_name):
+    file_list = []
+    with open(file_name + '.txt') as f:
+        for line in f:
+            line = line.strip().split('-')
+            file_list.append({"Title": line[0], "Type": line[1], "Source": line[2], "Link": '-'.join(line[3:])})
+
+    output_name = {topic_name: file_list}
+    out_file = open(file_name + '.json', "w") 
+    json.dump(output_name, out_file, indent = 4, sort_keys = False) 
+    out_file.close()
+    return json.dumps(output_name)
+
+# process_text_to_json(file_name = 'miscellaneous', topic_name = 'Miscellaneous')
+
+
+# merge jsons files
+result = []
+for f in glob.glob("*.json"):
+    with open(f, "rb") as infile:
+        result.append(json.load(infile))
+
+with open("merged_file.json", "wb") as outfile:
+        json.dump(result, outfile)
