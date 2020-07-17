41602cdcb82cd4401807bd44a75f3e1d5983634c
giorgiomondauto <gmondauto@gmail.com>
2020-07-17 09:50:12 +0100

    Update useful_functions.py

--- useful_functions.py
+++ useful_functions.py
@@ -247,5 +247,25 @@ def largest_sub(values):
     return max(result.items(), key = lambda x: x[1])
 
 
-print(largest_sub([1, 9, 3, 10, 4, 20, 2]))
+
+# Subarray - Subsequence - Subset
+# Consider an array:
+
+#  {1,2,3,4}
+# Subarray: contiguous sequence in an array i.e.
+
+# {1,2},{1,2,3}
+# Subsequence: Need not to be contiguous, but maintains order i.e.
+
+# {1,2,4}
+# Subset: Same as subsequence except it has empty set i.e.
+
+#  {1,3},{}
+# Given an array/sequence of size n, possible
+
+# Subarray = n*(n+1)/2
+# Subseqeunce = (2^n) -1 (non-empty subsequences)
+# Subset = 2^n
+
+# print(largest_sub([1, 9, 3, 10, 4, 20, 2]))
 
