068ef6f51669c170af7679e04b4bee8b88580dbe
giorgiomondauto <gmondauto@gmail.com>
2020-08-10 15:44:12 +0100

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,172 +1,68 @@
-####### LINKED LIST ########
+# Linked Lists
+# page 176/540
 
-# below: create a linkedlist, add new value, print list, reverse function
-class Node: 
-  
-    # Constructor to initialize the node object 
-    def __init__(self, data): 
-        self.data = data 
-        self.next = None
-  
-class LinkedList: 
-  
-    # Function to initialize head 
-    def __init__(self): 
-        self.head = None
-  
-    # Function to reverse the linked list 
-    def reverse(self): 
-        prev = None
-        current = self.head 
-        while(current is not None): 
-            next = current.next
-            current.next = prev 
-            prev = current 
-            current = next
-        self.head = prev 
-          
-    # Function to insert a new node at the beginning 
-    def push(self, new_data): 
-        new_node = Node(new_data) 
-        new_node.next = self.head 
-        self.head = new_node 
-  
-    # Utility function to print the linked LinkedList 
-    def printList(self): 
-        temp = self.head 
-        while(temp): 
-            print (temp.data) 
-            temp = temp.next
-            
-# Driver program to test above functions 
-llist = LinkedList() 
-llist.push(20) 
-llist.push(4) 
-llist.push(15) 
-llist.push(85) 
-  
-print ("Given Linked List")
-llist.printList() 
-llist.reverse() 
-print ("\nReversed Linked List")
-llist.printList() 
-
-#########################################################################################################################
-class linkedListNode:
-    def __init__(self, value, nextNode=None):
+class linkedlist:
+    '''
+    '''
+    def __init__(self,value,next_node = None):
+        '''
+        '''
         self.value = value
-        self.nextNode = nextNode
-
-def insertNode(head, valuetoInsert):
-    currentNode = head
-    while currentNode is not None:
-        if currentNode.nextNode is None:
-            currentNode.nextNode = linkedListNode(valuetoInsert)
-            return head
-        currentNode = currentNode.nextNode
-
-# Delete node function
-def deleteNode(head, valueToDelete):
-    currentNode = head
-    previousNode = None
-    while currentNode is not None:
-        if currentNode.value == valueToDelete:
-            if previousNode is None:
-                newHead = currentNode.nextNode
-                currentNode.nextNode = None
-                return newHead
-            previousNode.nextNode = currentNode.nextNode
-            return head
-        previousNode = currentNode
-        currentNode = currentNode.nextNode
-    return head  # Value to delete was not found.
-
-# "3" -> "7" -> "10"
-
-node1 = linkedListNode("3") # "3"
-node2 = linkedListNode("7") # "7"
-node3 = linkedListNode("10") # "10"
-
-node1.nextNode = node2 # node1 -> node2 , "3" -> "7"
-node2.nextNode = node3 # node2 -> node3 , "7" -> "10"
-
-# node1 -> node2 -> node3
-
-head = node1
-print ("*********************************")
-print ("Traversing the regular linkedList")
-print ("*********************************")
-# Regular Traversal
-currentNode = head
-while currentNode is not None:
-    print (currentNode.value),
-    currentNode = currentNode.nextNode
-print ('')
-print( "*********************************")
-
-print ("deleting the node '7'")
-newHead = deleteNode(head, "10")
-
-print ("*********************************")
-print ("traversing the new linkedList with the node 7 removed")
-print ("*********************************")
-
-currentNode = newHead
-while currentNode is not None:
-    print(currentNode.value),
-    currentNode = currentNode.nextNode
-
-print ('')
-print ("*********************************")
-print ("Inserting the node '99'")
-newHead = insertNode(newHead, "99")
-
-print ("*********************************")
-print ("traversing the new linkedList with the node 99 added")
-print ("*********************************")
-
-currentNode = newHead
-while currentNode is not None:
-    print (currentNode.value),
-    currentNode = currentNode.nextNode
-    
- ####### FIND MIDDLE OF A LINKED LIST ########
-class Node: 
-    def __init__(self, value): 
-        self.data = value 
-        self.next = None
-      
-class LinkedList: 
-  
-    def __init__(self): 
-        self.head = None
-  
-    # create Node and and make linked list 
-    def push(self, new_data): 
-        new_node = Node(new_data) 
-        new_node.next = self.head 
-        self.head = new_node 
-          
-    def printMiddle(self): 
-        temp = self.head  
-        count = 0
-          
-        while self.head: 
-  
-            # only update when count is odd 
-            if (count & 1):  
-                temp = temp.next
-            self.head = self.head.next
-  
-            # increment count in each iteration  
-            count += 1 
-          
-        print(temp.data)  
+        self.next_node = next_node
         
-list1 = LinkedList() 
-list1.push(5) 
-list1.push(4) 
-list1.push(2) 
-list1.push(3) 
-list1.push(1) 
-list1.printMiddle()
+        
+a = linkedlist(value = 10)
+b = linkedlist(value = 5)
+c = linkedlist(value = 15)
+
+a.next_node = b
+b.next_node = c
+
+head = a
+current_node = head
+while current_node is not None:
+    print(current_node.value)
+    current_node = current_node.next_node
+        
+print(10*'#')
+# SEARCHING NODE
+# The linked list search operation requires O(n) in the worst case, which occurs when
+# the target item is not in the list.
+def searching_node(head,target):
+    current_node = head
+    while current_node is not None and current_node.value != target:
+        current_node = current_node.next_node
+    return current_node
+
+print(searching_node(a,5))
+print(10*'#')
+
+# ADDING A NEW NODE AS HEAD
+d = linkedlist(value = 20)
+d.next_node = head # a
+head = d
+
+current_node = head
+while current_node is not None:
+    print(current_node.value)
+    current_node = current_node.next_node
+    
+print(10*'#')
+# DELETING A NODE
+predNode = None
+current_node = head
+target = 5
+while current_node is not None and current_node.value != target:
+    predNode = current_node
+    current_node = current_node.next_node
+
+if current_node is not None:
+    if current_node is head:
+        head = current_node.next_node
+    else:
+        predNode.next_node = current_node.next_node
+
+current_node = head
+while current_node is not None:
+    print(current_node.value)
+    current_node = current_node.next_node
