da2eaf30d9bc22b2d1be11e6b9d06640e31b629e
giorgiomondauto <gmondauto@gmail.com>
2020-10-22 08:35:12 +0100

    Update AlgoExpert.py

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -130,5 +130,24 @@ def calculateBranchSums(node,runningsum,sums):
 	calculateBranchSums(node.left,newrunningsums,sums)
 	calculateBranchSums(node.right,newrunningsums,sums)
 
+# Function 6
+# Selection Sort
+def selectionSort(array):
+    currentIdx = 0
+    while currentIdx < len(array)-1:
+        smallestIdx = currentIdx
+        for i in range(currentIdx + 1, len(array)):
+            if array[smallestIdx] > array[i]:
+                smallestIdx = i
+        swap(currentIdx, smallestIdx, array)
+        currentIdx +=1
+    return array
+
+
+def swap(i,j,array):
+    array[i],array[j] = array[j],array[i]
+    
+    
+print(selectionSort([1,5,4,77,5555]))
 	
     
