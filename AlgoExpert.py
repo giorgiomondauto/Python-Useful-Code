e309237a8e07b8de1a7b79b70f552c91b7654ba8
giorgiomondauto <gmondauto@gmail.com>
2020-10-22 08:08:33 +0100

    Update AlgoExpert.py

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -100,4 +100,30 @@ def swap(i,j,array):
 
 values = [1,5,8,33,4]
 print(insertionSort(values))
+
+# Function 5
+# BranchSums
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+		
+def branchSums(root):
+	sums = []
+	calculateBranchSums(root,0,sums)
+	return sums
+
+def calculateBranchSums(node,runningSum,sums):
+	if node is None:
+		return 
+	
+	newRunningSum = runningSum + node.value
+	if node.left is None and node.right is None:
+		sums.append(newRunningSum)
+		return
+	
+	calculateBranchSums(node.left, newRunningSum,sums)
+	calculateBranchSums(node.right,newRunningSum,sums)
     
