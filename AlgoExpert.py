120cf9ab10185ba1c44799fc8c079b4fa9aa1983
giorgiomondauto <gmondauto@gmail.com>
2020-10-26 09:26:51 +0000

    Update AlgoExpert.py

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -281,3 +281,17 @@ def fourNumberSum(array, targetSum):
 				   if array[i] + array[j] +array[z] +array[m] ==targetSum]
 	
 	return quadruplets
+
+# function 15
+# levenshteinDistance
+def levenshteinDistance(str1,str2):
+	edits = [[x for x in range(len(str1)+1)] for y in range(len(str2)+1)]
+	for i in range(1, len(str2)+1):
+		edits[i][0] = edits[i-1][0]+1
+	for i in range(1,len(str2)+1):
+		for j in range(1,len(str1)+1):
+			if str2[i-1]==str1[j-1]:
+				edits[i][j] = edits[i-1][j-1]
+			else:
+				edits[i][j] = 1+min(edits[i-1][j-1],edits[i-1][j],edits[i][j-1])
+	return edits[-1][-1]
