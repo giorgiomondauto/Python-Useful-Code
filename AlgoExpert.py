904c1f3b45591e7e27c06ba298464774d7bf4793
giorgiomondauto <gmondauto@gmail.com>
2020-10-27 08:01:07 +0000

    Update AlgoExpert.py

--- AlgoExpert.py
+++ AlgoExpert.py
@@ -302,13 +302,15 @@ def levenshteinDistance(str1,str2):
 def maxSubsetSumNoAdjacent(array):
     # Write your code here.
     if len(array)==0:
-	return 0
+        return 0
     elif len(array)==1:
-	 return array[0]
-	
+        return array[0]
+    
     maxSums = array[:]
     maxSums[1] = max(array[0], array[1])
     for i in range(2,len(array)):
-        maxSums[i] = max(maxSums[i-1], maxSums[i-2] + array[i])
-		
+        maxSums[i] = max(maxSums[i-1], maxSums[i-2] + maxSums[i])
+        # print( max(maxSums[i-1], maxSums[i-2] + maxSums[i]))
+        # print(maxSums)
+        # print(10*'-')
     return maxSums[-1]
